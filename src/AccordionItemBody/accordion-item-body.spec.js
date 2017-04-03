9d38896dce5ca3821a50e43a3bf0e14b7811fd83
Vincent Audebert <vincent.audebert@yahoo.fr>
2017-04-03 12:20:10 +1200

    adds tests for accordion item body

---
+++ src/AccordionItemBody/accordion-item-body.spec.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import AccordionItemBody from './accordion-item-body';
+
+describe('AccordionItemBody', () => {
+    it('renders correctly with min params', () => {
+        const tree = renderer.create(
+            <AccordionItemBody>
+                <div>Fake body</div>
+            </AccordionItemBody>,
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders correctly with different className', () => {
+        const tree = renderer.create(
+            <AccordionItemBody className="testCSSClass">
+                <div>Fake body</div>
+            </AccordionItemBody>,
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders correctly with prefixClass', () => {
+        const tree = renderer.create(
+            <AccordionItemBody prefixCss="testCSSClass">
+                <div>Fake body</div>
+            </AccordionItemBody>,
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders correctly with expanded', () => {
+        const tree = renderer.create(
+            <AccordionItemBody expanded={true}>
+                <div>Fake body</div>
+            </AccordionItemBody>,
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
