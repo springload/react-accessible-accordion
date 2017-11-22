10b29b4b5bb4b93a63307e8208f89a14c2a2771b
Vincent Audebert <vincent.audebert@yahoo.fr>
2017-11-22 14:52:09 +1300

    WIP adds unit test

--- src/Accordion/accordion.spec.js
+++ src/Accordion/accordion.spec.js
@@ -1,41 +1,51 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import renderer from 'react-test-renderer';
 
 import Accordion from './accordion';
 import AccordionItem from '../AccordionItem/accordion-item';
+import AccordionItemTitle from '../AccordionItemTitle/accordion-item-title';
+import AccordionItemBody from '../AccordionItemBody/accordion-item-body';
 
 jest.mock('../AccordionItem/accordion-item', () => 'div');
+jest.mock('../AccordionItemTitle/accordion-item-title', () => 'div');
+jest.mock('../AccordionItemBody/accordion-item-body', () => 'div');
 
 describe('Accordion', () => {
     it('renders correctly with min params', () => {
-        const tree = renderer.create(
-            <Accordion>
-                <AccordionItem>Fake child</AccordionItem>
-                <AccordionItem>Fake child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion>
+                    <AccordionItem>Fake child</AccordionItem>
+                    <AccordionItem>Fake child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('renders correctly with accordion false', () => {
-        const tree = renderer.create(
-            <Accordion accordion={false}>
-                <AccordionItem>Fake child</AccordionItem>
-                <AccordionItem>Fake child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion accordion={false}>
+                    <AccordionItem>Fake child</AccordionItem>
+                    <AccordionItem>Fake child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('different className', () => {
-        const tree = renderer.create(
-            <Accordion accordion={false} className="testCSSClass">
-                <AccordionItem>Fake Child</AccordionItem>
-                <AccordionItem>Fake Child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion accordion={false} className="testCSSClass">
+                    <AccordionItem>Fake Child</AccordionItem>
+                    <AccordionItem>Fake Child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
@@ -88,73 +98,87 @@ describe('Accordion', () => {
     });
 
     it('pre expanded accordion', () => {
-        const tree = renderer.create(
-            <Accordion>
-                <AccordionItem expanded={true}>Fake Child</AccordionItem>
-                <AccordionItem>Fake Child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion>
+                    <AccordionItem expanded={true}>Fake Child</AccordionItem>
+                    <AccordionItem>Fake Child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('works with multiple pre expanded accordion. Extra expands are just ignored.', () => {
-        const tree = renderer.create(
-            <Accordion>
-                <AccordionItem expanded={true}>Fake Child</AccordionItem>
-                <AccordionItem expanded={true}>Fake Child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion>
+                    <AccordionItem expanded={true}>Fake Child</AccordionItem>
+                    <AccordionItem expanded={true}>Fake Child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('pre expanded accordion when accordion is false', () => {
-        const tree = renderer.create(
-            <Accordion accordion={false}>
-                <AccordionItem expanded={true}>Fake Child</AccordionItem>
-                <AccordionItem expanded={true}>Fake Child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion accordion={false}>
+                    <AccordionItem expanded={true}>Fake Child</AccordionItem>
+                    <AccordionItem expanded={true}>Fake Child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('pre expand accordion via accordion props', () => {
-        const tree = renderer.create(
-            <Accordion activeItems={[0]}>
-                <AccordionItem>Fake Child</AccordionItem>
-                <AccordionItem>Fake Child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion activeItems={[0]}>
+                    <AccordionItem>Fake Child</AccordionItem>
+                    <AccordionItem>Fake Child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('pre expand accordion via accordion props vs accordion item props. Expanded only second item.', () => {
-        const tree = renderer.create(
-            <Accordion activeItems={[0]}>
-                <AccordionItem>Fake Child</AccordionItem>
-                <AccordionItem expanded={true}>Fake Child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion activeItems={[0]}>
+                    <AccordionItem>Fake Child</AccordionItem>
+                    <AccordionItem expanded={true}>Fake Child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('pre expand multiple accordions via accordion props', () => {
-        const tree = renderer.create(
-            <Accordion accordion={false} activeItems={[0, 2]}>
-                <AccordionItem>Fake Child</AccordionItem>
-                <AccordionItem>Fake Child</AccordionItem>
-                <AccordionItem>Fake Child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion accordion={false} activeItems={[0, 2]}>
+                    <AccordionItem>Fake Child</AccordionItem>
+                    <AccordionItem>Fake Child</AccordionItem>
+                    <AccordionItem>Fake Child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('pre expand accordion via accordion props with custom key', () => {
-        const tree = renderer.create(
-            <Accordion activeItems={['custom']}>
-                <AccordionItem>Fake Child</AccordionItem>
-                <AccordionItem customKey="custom">Fake Child</AccordionItem>
-            </Accordion>,
-        ).toJSON();
+        const tree = renderer
+            .create(
+                <Accordion activeItems={['custom']}>
+                    <AccordionItem>Fake Child</AccordionItem>
+                    <AccordionItem customKey="custom">Fake Child</AccordionItem>
+                </Accordion>,
+            )
+            .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
@@ -196,23 +220,23 @@ describe('Accordion', () => {
 
     it(`expand multiple accordions via accordion props props dynamicly with default
         expanded on accordion items`, () => {
-            const wrapper = renderer.create(
-                <Accordion accordion={false}>
-                    <AccordionItem expanded={true}>Fake Child</AccordionItem>
-                    <AccordionItem>Fake Child</AccordionItem>
-                    <AccordionItem>Fake Child</AccordionItem>
-                </Accordion>,
-            );
-            wrapper.update(
-                <Accordion accordion={false} activeItems={[1, 2]}>
-                    <AccordionItem expanded={true}>Fake Child</AccordionItem>
-                    <AccordionItem>Fake Child</AccordionItem>
-                    <AccordionItem>Fake Child</AccordionItem>
-                </Accordion>,
-            );
-            expect(wrapper.getInstance().state.activeItems).toEqual([1, 2]);
-            expect(wrapper).toMatchSnapshot();
-        });
+        const wrapper = renderer.create(
+            <Accordion accordion={false}>
+                <AccordionItem expanded={true}>Fake Child</AccordionItem>
+                <AccordionItem>Fake Child</AccordionItem>
+                <AccordionItem>Fake Child</AccordionItem>
+            </Accordion>,
+        );
+        wrapper.update(
+            <Accordion accordion={false} activeItems={[1, 2]}>
+                <AccordionItem expanded={true}>Fake Child</AccordionItem>
+                <AccordionItem>Fake Child</AccordionItem>
+                <AccordionItem>Fake Child</AccordionItem>
+            </Accordion>,
+        );
+        expect(wrapper.getInstance().state.activeItems).toEqual([1, 2]);
+        expect(wrapper).toMatchSnapshot();
+    });
 
     it('close accordions via accordion props props dynamicly', () => {
         const wrapper = renderer.create(
@@ -245,5 +269,71 @@ describe('Accordion', () => {
             </Accordion>,
         );
         expect(wrapper).toMatchSnapshot();
+    });
+
+    it('different className with the same activeItems prop', () => {
+        const wrapper = renderer.create(
+            <Accordion activeItems={[1]} className="test-1">
+                <AccordionItem>Fake Child</AccordionItem>
+                <AccordionItem>Fake Child</AccordionItem>
+            </Accordion>,
+        );
+        wrapper.update(
+            <Accordion activeItems={[1]} className="test-2">
+                <AccordionItem>Fake Child</AccordionItem>
+                <AccordionItem>Fake Child</AccordionItem>
+            </Accordion>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('supports controlled component inside accordion', () => {
+        class App extends Component {
+            constructor(props) {
+                super(props);
+
+                this.state = {
+                    value: '',
+                };
+            }
+
+            handleChange = evt => {
+                this.setState({
+                    value: evt.target.value,
+                });
+            };
+
+            render() {
+                return (
+                    <Accordion activeItems={[1]} id="accordion">
+                        <AccordionItem>
+                            <AccordionItemTitle>
+                                Open and try to write something in the input
+                            </AccordionItemTitle>
+                            <AccordionItemBody>
+                                <input
+                                    id="controlled-input"
+                                    onChange={this.handleChange}
+                                    value={this.state.value}
+                                />
+                            </AccordionItemBody>
+                        </AccordionItem>
+                    </Accordion>
+                );
+            }
+        }
+        const wrapper = renderer.create(<App />);
+        const accordion = wrapper.root.findByType(Accordion);
+        accordion._fiber.stateNode.handleClick(0);
+        console.log(accordion._fiber.stateNode.state);
+        expect(accordion._fiber.stateNode.state.activeItems).toEqual([0]);
+        wrapper.getInstance().handleChange({ target: { value: 'newValue' } });
+        console.log(accordion._fiber.stateNode.state);
+        expect(accordion._fiber.stateNode.state.activeItems).toEqual([0]);
+
+        // accordion.getInstance().handleClick(0);
+        // expect(accordion.getInstance().state.activeItems).toEqual([0]);
+
+        // expect(accordion.getInstance().state.activeItems).toEqual([0]);
     });
 });
