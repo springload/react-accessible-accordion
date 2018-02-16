0db093163787def07e3f0d7a367fdeeb532a8a39
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-02-16 15:52:47 +1300

    Remove commented code (and unused prop) in Accordion

--- src/Accordion/accordion.js
+++ src/Accordion/accordion.js
@@ -7,7 +7,7 @@ import { createAccordionStore } from '../accordionStore/accordionStore';
 type AccordionProps = {
     accordion: boolean,
     children: Node,
-    activeItems: Array<string | number>,
+    // activeItems: Array<string | number>,
     className: string,
     onChange: Function,
 };
@@ -24,79 +24,6 @@ class Accordion extends Component<AccordionProps, *> {
         accordion: this.props.accordion,
         onChange: this.props.onChange,
     });
-
-    // componentDidUpdate() {
-    //     this.accordionStore.items = this.parseItems();
-    // }
-
-    // parseItems() {
-    //     let items = [];
-    //     let hasOneExpanded = false;
-    //     let expandsWithChildrenProperties = false;
-
-    //     // we leave priority to children properties
-    //     React.Children.map(this.props.children, (child, index) => {
-    //         const item = {
-    //             itemkey: child.props.customKey || index,
-    //             itemUuid: nextUuid(),
-    //             expanded: false,
-    //         };
-
-    //         if (this.props.accordion) {
-    //             item.expanded = !hasOneExpanded && child.props.expanded;
-    //             if (child.props.expanded) {
-    //                 hasOneExpanded = true;
-    //             }
-    //         } else {
-    //             item.expanded = child.props.expanded;
-    //         }
-
-    //         if (child.props.expanded) {
-    //             expandsWithChildrenProperties = true;
-    //         }
-    //         items.push(item);
-    //     });
-
-    //     // if not using children properties to expand items we check with activeItems property
-    //     if (
-    //         !expandsWithChildrenProperties &&
-    //         this.props.activeItems.length !== 0
-    //     ) {
-    //         items = items.map((item: Object) => {
-    //             const resetItem = item;
-    //             resetItem.expanded = false;
-    //             return resetItem;
-    //         });
-
-    //         this.props.activeItems.forEach(activeItem => {
-    //             const foundItem = items.find(
-    //                 item => item.itemkey === activeItem,
-    //             );
-    //             hasOneExpanded = false;
-    //             if (foundItem) {
-    //                 let expanded = true;
-    //                 if (this.props.accordion) {
-    //                     expanded = !hasOneExpanded;
-    //                     hasOneExpanded = true;
-    //                 }
-    //                 foundItem.expanded = expanded;
-    //             }
-    //         });
-    //     }
-    //     return items;
-    // }
-
-    // renderItems() {
-    //     const { children } = this.props;
-
-    //     return React.Children.map(children, (item, index) => {
-    //         const itemkey = item.props.customKey || index;
-
-    //         return React.cloneElement(item, {
-    //             itemkey,
-    //         });
-    //     });
-    // }
 
     render() {
         const { className, children } = this.props;
