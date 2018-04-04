662d321b1dc5210819ade12c133aa9a79480ee30
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-04-04 20:33:38 +1200

    Re-enable and fix all tests in accordion-item-body.spec

--- src/AccordionItemBody/accordion-item-body.spec.js
+++ src/AccordionItemBody/accordion-item-body.spec.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import { mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { Provider } from 'unstated';
 import ItemContainer, { resetNextUuid } from '../ItemContainer/ItemContainer';
 import AccordionContainer from '../AccordionContainer/AccordionContainer';
@@ -44,47 +44,47 @@ describe('AccordionItemBody', () => {
         expect(tree).toMatchSnapshot();
     });
 
-    // it('renders correctly with different className', () => {
-    //     const className = 'className';
-    //     const wrapper = mount(
-    //         <Provider accordionStore={accordionStore} uuid={uuid}>
-    //             <AccordionItemBody className={className} />
-    //         </Provider>,
-    //     );
-    //     expect(wrapper.find('div').hasClass(className)).toEqual(true);
-    // });
+    it('renders correctly with different className', () => {
+        const className = 'className';
+        const wrapper = mount(
+            <Provider inject={[accordionStore, itemStore]}>
+                <AccordionItemBody className={className} />
+            </Provider>,
+        );
+        expect(wrapper.find('div').hasClass(className)).toEqual(true);
+    });
 
-    // it('renders correctly with different hideBodyClassName', () => {
-    //     const hideBodyClassName = 'hideBodyClassName';
-    //     const wrapper = mount(
-    //         <Provider accordionStore={accordionStore} uuid={uuid}>
-    //             <AccordionItemBody hideBodyClassName={hideBodyClassName} />
-    //         </Provider>,
-    //     );
-    //     expect(wrapper.find('div').hasClass(hideBodyClassName)).toEqual(true);
-    // });
+    it('renders correctly with different hideBodyClassName', () => {
+        const hideBodyClassName = 'hideBodyClassName';
+        const wrapper = mount(
+            <Provider inject={[accordionStore, itemStore]}>
+                <AccordionItemBody hideBodyClassName={hideBodyClassName} />
+            </Provider>,
+        );
+        expect(wrapper.find('div').hasClass(hideBodyClassName)).toEqual(true);
+    });
 
-    // it('renders null if an associated AccordionItem is not registered in accordionStore', () => {
-    //     const className = 'className';
-    //     const wrapper = mount(
-    //         <Provider accordionStore={accordionStore} uuid="foo">
-    //             <AccordionItemBody className={className}>
-    //                 <div>Fake body</div>
-    //             </AccordionItemBody>
-    //         </Provider>,
-    //     );
+    it('renders null if an associated AccordionItem is not registered in accordionStore', () => {
+        const className = 'className';
+        const wrapper = mount(
+            <Provider inject={[accordionStore, itemStore]}>
+                <AccordionItemBody className={className}>
+                    <div>Fake body</div>
+                </AccordionItemBody>
+            </Provider>,
+        );
 
-    //     expect(
-    //         wrapper.findWhere(item => item.className === className).length,
-    //     ).toEqual(0);
-    // });
+        expect(
+            wrapper.findWhere(item => item.className === className).length,
+        ).toEqual(0);
+    });
 
-    // it('respects arbitrary user-defined props', () => {
-    //     const wrapper = mount(
-    //         <Provider accordionStore={accordionStore} uuid={uuid}>
-    //             <AccordionItemBody lang="en" />
-    //         </Provider>,
-    //     );
-    //     expect(wrapper.find('div').instance().lang).toEqual('en');
-    // });
+    it('respects arbitrary user-defined props', () => {
+        const wrapper = mount(
+            <Provider inject={[accordionStore, itemStore]}>
+                <AccordionItemBody lang="en" />
+            </Provider>,
+        );
+        expect(wrapper.find('div').instance().lang).toEqual('en');
+    });
 });
