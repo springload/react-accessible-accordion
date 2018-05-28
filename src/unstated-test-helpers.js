c9e4a125d4b898f2010a1544192058b8d830e5e3
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-05-28 22:17:07 +1200

    Fix linting errors in unstated-test-helpers

--- src/unstated-test-helpers.js
+++ src/unstated-test-helpers.js
@@ -1,6 +1,8 @@
 // @flow
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { mount, type ReactWrapper } from 'enzyme';
+import { type Node } from 'react';
 import { Container } from 'unstated';
 
 const spy = jest.spyOn(Container.prototype, 'setState');
@@ -9,6 +11,7 @@ const spy = jest.spyOn(Container.prototype, 'setState');
  * Keeps a ledger of *all* calls to unstated's `setState` method, and resolves when they all do.
  */
 export async function setStateComplete(wrapper: ReactWrapper) {
+    // $FlowFixMe
     await Promise.all(spy.mock.results);
 
     wrapper.update();
@@ -17,7 +20,7 @@ export async function setStateComplete(wrapper: ReactWrapper) {
 /*
  * To be used instead of 'mount'. Waits until all `setState` calls have resolved before returning the node.
  */
-export async function mountComplete(node) {
+export async function mountComplete(node: Node) {
     const mounted = mount(node);
 
     await setStateComplete(mounted);
