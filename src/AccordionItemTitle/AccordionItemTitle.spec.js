ae6a81be63eb32e016d6f081b762b48d6011ef20
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-09-24 11:22:29 +0200

    Refactor unstated out of AccordionItemTitle.spec

--- src/AccordionItemTitle/AccordionItemTitle.spec.js
+++ src/AccordionItemTitle/AccordionItemTitle.spec.js
@@ -2,149 +2,120 @@
 
 import React from 'react';
 import { mount } from 'enzyme';
-import { Provider } from 'unstated';
 import AccordionItemTitle from './AccordionItemTitle.wrapper';
-import ItemContainer, { resetNextUuid } from '../ItemContainer/ItemContainer';
-import AccordionContainer from '../AccordionContainer/AccordionContainer';
+import { Provider as ItemProvider } from '../ItemContainer/ItemContainer';
+import { Provider as AccordionProvider } from '../AccordionContainer/AccordionContainer';
 
 describe('AccordionItemTitle', () => {
-    let accordionContainer;
-    let itemContainer;
-    let setExpandedSpy;
+    const DEFAULT_ITEM = {
+        uuid: 0,
+        expanded: false,
+        disabled: false,
+    };
 
-    beforeEach(() => {
-        resetNextUuid();
+    function mountItem(node, item = DEFAULT_ITEM) {
+        return mount(
+            <AccordionProvider accordion={false} items={[item]}>
+                <ItemProvider uuid={item.uuid}>{node}</ItemProvider>
+            </AccordionProvider>,
+        );
+    }
 
-        itemContainer = new ItemContainer();
-        accordionContainer = new AccordionContainer({
-            accordion: false,
-            items: [
-                {
-                    uuid: 0, // because `nextUuid` in ItemContainer starts at zero.
-                    expanded: false,
-                    disabled: false,
-                },
-            ],
-        });
-
-        setExpandedSpy = jest.spyOn(accordionContainer, 'setExpanded');
-    });
+    function isExpanded(wrapper, uuid) {
+        return !!wrapper
+            .find(AccordionProvider)
+            .instance()
+            .state.items.find(item => item.uuid === uuid).expanded;
+    }
 
     it('renders correctly with min params', () => {
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle>
-                    <div>Fake Title</div>
-                </AccordionItemTitle>
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle>
+                <div>Fake Title</div>
+            </AccordionItemTitle>,
         );
         expect(wrapper).toMatchSnapshot();
     });
 
     it('renders correctly with different className', () => {
         const className = 'className';
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle className={className} />
-            </Provider>,
-        );
+        const wrapper = mountItem(<AccordionItemTitle className={className} />);
         expect(wrapper.find('div').hasClass(className)).toEqual(true);
     });
 
     it('renders with different hideBodyClassName', () => {
         const hideBodyClassName = 'hideBodyClassName';
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle hideBodyClassName={hideBodyClassName} />
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle hideBodyClassName={hideBodyClassName} />,
         );
         expect(wrapper.find('div').hasClass(hideBodyClassName)).toEqual(true);
     });
 
     it("doesn't present hideBodyClassName when collapsed", () => {
         const hideBodyClassName = 'hideBodyClassName';
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle hideBodyClassName={hideBodyClassName} />
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle hideBodyClassName={hideBodyClassName} />,
         );
         expect(wrapper.find('div').hasClass(hideBodyClassName)).toEqual(true);
     });
 
     it('toggles state when clicked', async () => {
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle>Fake Title</AccordionItemTitle>
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle>Fake Title</AccordionItemTitle>,
         );
 
-        expect(setExpandedSpy).not.toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
         wrapper.find('div').simulate('click');
-        expect(setExpandedSpy).toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeTruthy();
     });
 
     it('doesn’t toggle state when trying to click but disabled', async () => {
-        await accordionContainer.removeItem(0);
-        await accordionContainer.addItem({
-            uuid: 0, // because `nextUuid` in ItemContainer starts at zero.
-            expanded: false,
-            disabled: true,
-        });
-
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle>Fake Title</AccordionItemTitle>
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle>Fake Title</AccordionItemTitle>,
+            {
+                ...DEFAULT_ITEM,
+                disabled: true,
+            },
         );
 
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
         wrapper.find('div').simulate('click');
-
-        expect(setExpandedSpy).not.toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
     });
 
     it('toggles state when pressing enter', async () => {
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle>Fake Title</AccordionItemTitle>
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle>Fake Title</AccordionItemTitle>,
         );
 
-        expect(setExpandedSpy).not.toHaveBeenCalled();
-
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
         wrapper.find('div').simulate('keypress', { charCode: 13 });
-
-        expect(setExpandedSpy).toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeTruthy();
     });
 
     it('toggles state when pressing space', async () => {
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle>Fake Title</AccordionItemTitle>
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle>Fake Title</AccordionItemTitle>,
         );
 
-        expect(setExpandedSpy).not.toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
         wrapper.find('div').simulate('keypress', { charCode: 32 });
-        expect(setExpandedSpy).toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeTruthy();
     });
 
     it('doesn’t toggle state when pressing another key', async () => {
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle>Fake Title</AccordionItemTitle>
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle>Fake Title</AccordionItemTitle>,
         );
 
-        expect(setExpandedSpy).not.toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
         wrapper.find('div').simulate('keypress', { charCode: 35 });
-        expect(setExpandedSpy).not.toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
     });
 
     it('respects arbitrary user-defined props', () => {
-        const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle lang="en">Fake Title</AccordionItemTitle>
-            </Provider>,
+        const wrapper = mountItem(
+            <AccordionItemTitle lang="en">Fake Title</AccordionItemTitle>,
         );
 
         expect(wrapper.find('div').instance().lang).toEqual('en');
@@ -152,22 +123,24 @@ describe('AccordionItemTitle', () => {
 
     // edge case to cover branch
     it('doesn’t toggle state when clicking but disabled & accordion === true', async () => {
-        await accordionContainer.setAccordion(true);
-        await accordionContainer.removeItem(0);
-        await accordionContainer.addItem({
-            uuid: 0, // because `nextUuid` in ItemContainer starts at zero.
-            expanded: false,
-            disabled: true,
-        });
-
         const wrapper = mount(
-            <Provider inject={[accordionContainer, itemContainer]}>
-                <AccordionItemTitle>Fake Title</AccordionItemTitle>
-            </Provider>,
+            <AccordionProvider
+                accordion
+                items={[
+                    {
+                        ...DEFAULT_ITEM,
+                        disabled: true,
+                    },
+                ]}
+            >
+                <ItemProvider uuid={DEFAULT_ITEM.uuid}>
+                    <AccordionItemTitle>Fake Title</AccordionItemTitle>
+                </ItemProvider>
+            </AccordionProvider>,
         );
 
-        expect(setExpandedSpy).not.toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
         wrapper.find('div').simulate('click');
-        expect(setExpandedSpy).not.toHaveBeenCalled();
+        expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
     });
 });
