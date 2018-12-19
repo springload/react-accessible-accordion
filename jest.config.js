8f601d8444bbcfd127f08ae252f18bdde9b97c6b
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-12-20 09:13:19 +1300

    Configure jest to deal with TS tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
 	setupFiles: ['./src/setupTests.js'],
 	setupTestFrameworkScriptFile: './src/setupTestFramework.js',
 	testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+	moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
 	transform: {
-		'^.+\\.jsx?$': 'babel-jest',
+		'^.+\\.(js|ts)x?$': 'babel-jest',
 	},
 };
