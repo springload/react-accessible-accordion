d3fe2475b61b422dc9aad388d8dc36cb0595e506
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-12-23 14:15:11 +1300

    Create 'CombineConsumers' helper class

---
+++ src/CombineConsumers.spec.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CombineConsumers from './CombineConsumers';
+
+describe('CombineConsumers', () => {
+	const FooConsumer = ({
+		children,
+	}: {
+		children(arg: 'foo'): React.ReactNode;
+	}) => children('foo');
+	const BarConsumer = ({
+		children,
+	}: {
+		children(arg: 'bar'): React.ReactNode;
+	}) => children('bar');
+
+	it('Passes props from both consumers', () => {
+		const wrapper = mount(
+			<CombineConsumers consumers={[FooConsumer, BarConsumer]}>
+				{(foo, bar) => <div>{`${foo}${bar}`}</div>}
+			</CombineConsumers>,
+		);
+
+		console.log(wrapper.debug());
+
+		expect(wrapper.find('div').text()).toEqual('foobar');
+	});
+});
