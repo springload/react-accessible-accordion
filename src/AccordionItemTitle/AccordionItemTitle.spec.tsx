ce194173a2f87a2eca71cc5ec73f771c723db100
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-01-02 16:38:13 +1300

    Refactor AccordionItemTitle.spec to TSX

--- src/AccordionItemTitle/AccordionItemTitle.spec.tsx
+++ src/AccordionItemTitle/AccordionItemTitle.spec.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { mount } from 'enzyme';
 import AccordionItemTitle from './AccordionItemTitle.wrapper';
 import { Provider as ItemProvider } from '../ItemContainer/ItemContainer';
