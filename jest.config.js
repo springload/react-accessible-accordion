07145a55c6c4be949d0b8f04f2d60f99b0629b51
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-01-24 09:41:57 +1300

    Ignore 'integration' folder

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
     setupFiles: ['./src/setupTests.js'],
     setupTestFrameworkScriptFile: './src/setupTestFramework.js',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/integration/'],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     transform: {
         '^.+\\.(js|ts)x?$': 'babel-jest',
