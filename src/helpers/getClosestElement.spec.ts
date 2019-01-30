e94aa8681316af490984ac256fac90f1b02cf377
Samantha Sanders <samantha.k.sanders@gmail.com>
2019-01-30 16:43:02 +1300

    Add a couple more tests for get closest element function

--- src/helpers/getClosestElement.spec.ts
+++ src/helpers/getClosestElement.spec.ts
@@ -15,4 +15,38 @@ describe('getClosestElement', () => {
             ancestor,
         );
     });
+
+    it('gets the closest ancestor several levels up', () => {
+        const ANCESTOR_ID = 'foo';
+        const DESCENDANT_ID = 'bar';
+
+        const ancestor = document.createElement('div');
+        ancestor.setAttribute('id', ANCESTOR_ID);
+
+        const middleMan = document.createElement('div');
+
+        const descendant = document.createElement('div');
+        descendant.setAttribute('id', DESCENDANT_ID);
+
+        middleMan.appendChild(descendant);
+        ancestor.appendChild(middleMan);
+
+        expect(getClosestElement(descendant, `#${ANCESTOR_ID}`)).toEqual(
+            ancestor,
+        );
+    });
+
+    it('no ancestor that matches selector', () => {
+        const ANCESTOR_ID = 'foo';
+        const DESCENDANT_ID = 'bar';
+
+        const ancestor = document.createElement('div');
+
+        const descendant = document.createElement('div');
+        descendant.setAttribute('id', DESCENDANT_ID);
+
+        ancestor.appendChild(descendant);
+
+        expect(getClosestElement(descendant, `#${ANCESTOR_ID}`)).toBeNull();
+    });
 });
