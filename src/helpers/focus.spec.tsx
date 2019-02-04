4351352cbf8b84225c385f2d921b52c1d10919d7
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-02-05 09:28:36 +1300

    Refactor enzyme out of focus specs

--- src/helpers/focus.spec.tsx
+++ src/helpers/focus.spec.tsx
@@ -1,73 +1,66 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
 
 import { getClosestAccordion } from './focus';
 
 describe('focus', () => {
-    function createAccordionElement() {
-        const element = document.createElement('div');
-        element.setAttribute('data-accordion-component', 'Accordion');
-        return element;
-    }
+    function createTree(innerHTML: string): DocumentFragment {
+        const template = document.createElement('template');
+        // tslint:disable-next-line:no-inner-html
+        template.innerHTML = innerHTML.trim();
 
-    function createHeadingElement() {
-        const element = document.createElement('div');
-        element.setAttribute(
-            'data-accordion-component',
-            'AccordionItemHeading',
-        );
+        return template.content;
     }
 
     describe('getClosestAccordion', () => {
         it('gets an ancestral accordion element', () => {
-            const tree = mount(
+            const tree = createTree(`
                 <div data-accordion-component="Accordion" id="parent">
-                    <div id="child" />
-                </div>,
-            );
+                   <div id="child">Child</div>
+                </div>
+            `);
 
-            const child = tree.find('#child').instance() as HTMLDivElement;
-            const parent = tree.find('#parent').instance() as HTMLDivElement;
+            const child = tree.querySelector('#child');
+            const parent = tree.querySelector('#parent');
 
             expect(child instanceof HTMLElement).toEqual(true);
             expect(parent instanceof HTMLElement).toEqual(true);
-            expect(getClosestAccordion(child)).toEqual(parent);
+            expect(getClosestAccordion(child as HTMLElement)).toEqual(parent);
         });
 
         it('only gets the nearest ancestral accordion element when there are more than one', () => {
-            const tree = mount(
+            const tree = createTree(`
                 <div data-accordion-component="Accordion">
                     <div data-accordion-component="Accordion" id="parent">
-                        <div id="child" />
+                        <div id="child">Child</div>
                     </div>
-                </div>,
-            );
+                </div>
+            `);
 
-            const child = tree.find('#child').instance();
-            const parent = tree.find('#parent').instance();
+            const child = tree.querySelector('#child');
+            const parent = tree.querySelector('#parent');
 
             expect(child instanceof HTMLElement).toEqual(true);
             expect(parent instanceof HTMLElement).toEqual(true);
-            expect(getClosestAccordion(child)).toEqual(parent);
+            expect(getClosestAccordion(child as HTMLElement)).toEqual(parent);
         });
 
         it('recurses through intermediary elements', () => {
-            const tree = mount(
+            const tree = createTree(`
                 <div data-accordion-component="Accordion" id="parent">
                     <div>
                         <div>
-                            <div id="child" />
+                            <div id="child">Child</div>
                         </div>
                     </div>
-                </div>,
-            );
+                </div>
+            `);
 
-            const child = tree.find('#child').instance();
-            const parent = tree.find('#parent').instance();
+            const child = tree.querySelector('#child');
+            const parent = tree.querySelector('#parent');
 
             expect(child instanceof HTMLElement).toEqual(true);
             expect(parent instanceof HTMLElement).toEqual(true);
-            expect(getClosestAccordion(child)).toEqual(parent);
+            expect(getClosestAccordion(child as HTMLElement)).toEqual(parent);
         });
     });
 });
