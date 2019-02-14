3fabfb1388d473c66bbfe492df066722365bc01a
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-02-15 10:26:15 +1300

    Fix tests in AccordionItemHeading

--- src/AccordionItemHeading/AccordionItemHeading.spec.tsx
+++ src/AccordionItemHeading/AccordionItemHeading.spec.tsx
@@ -4,135 +4,141 @@ import { Provider as AccordionProvider } from '../AccordionContext/AccordionCont
 import { Provider as ItemProvider, UUID } from '../ItemContext/ItemContext';
 import { default as AccordionItemHeading } from './AccordionItemHeading.wrapper';
 
-// describe('AccordionItemHeading', () => {
-//     enum Uuids {
-//         FOO = 'FOO',
-//         BAR = 'BAR',
-//     }
+describe('AccordionItemHeading', () => {
+    enum UUIDS {
+        FOO,
+        BAR,
+    }
 
-//     it('renders null outside the context of an ‘Accordion’', () => {
-//         const wrapper = mount(
-//             <ItemProvider uuid={Uuids.FOO}>
-//                 <AccordionItemHeading />
-//             </ItemProvider>,
-//         );
+    it('renders null outside the context of an ‘Accordion’', () => {
+        const wrapper = mount(
+            <ItemProvider uuid={UUIDS.FOO}>
+                <AccordionItemHeading />
+            </ItemProvider>,
+        );
 
-//         expect(wrapper.html()).toBeNull();
-//     });
+        expect(wrapper.html()).toBeNull();
+    });
 
-//     it('renders null outside the context of an ‘AccordionItem’', () => {
-//         const wrapper = mount(
-//             <AccordionProvider>
-//                 <AccordionItemHeading />
-//             </AccordionProvider>,
-//         );
+    it('renders null outside the context of an ‘AccordionItem’', () => {
+        const wrapper = mount(
+            <AccordionProvider>
+                <AccordionItemHeading />
+            </AccordionProvider>,
+        );
 
-//         expect(wrapper.html()).toBeNull();
-//     });
+        expect(wrapper.html()).toBeNull();
+    });
 
-//     function mountItem(
-//         node: JSX.Element,
-//         uuid: UUID = Uuids.FOO,
-//         expanded: boolean = false,
-//     ): ReactWrapper {
-//         return mount(
-//             <AccordionProvider
-//                 allowMultipleExpanded={true}
-//                 preExpanded={expanded ? [uuid] : undefined}
-//             >
-//                 <ItemProvider uuid={uuid}>{node}</ItemProvider>
-//             </AccordionProvider>,
-//         );
-//     }
+    function mountItem(
+        node: JSX.Element,
+        uuid: UUID = UUIDS.FOO,
+        expanded: boolean = false,
+    ): ReactWrapper {
+        const preExpanded = expanded ? [uuid] : undefined;
 
-//     function isExpanded(wrapper: ReactWrapper, uuid: string | number): boolean {
-//         const instance = wrapper
-//             .find(AccordionProvider)
-//             .instance() as AccordionProvider;
+        return mount(
+            <AccordionProvider
+                allowMultipleExpanded={true}
+                preExpanded={preExpanded}
+            >
+                <ItemProvider uuid={uuid}>{node}</ItemProvider>
+            </AccordionProvider>,
+        );
+    }
 
-//         return !!instance.state.items.find((item: Item) => item.uuid === uuid)
-//             .expanded;
-//     }
+    function isExpanded(wrapper: ReactWrapper, uuid: UUID): boolean {
+        const instance = wrapper.find(AccordionProvider).instance();
 
-//     it('renders correctly with min params', () => {
-//         const wrapper = mountItem(
-//             <AccordionItemHeading>
-//                 <div>Fake Title</div>
-//             </AccordionItemHeading>,
-//         );
-//         expect(wrapper).toMatchSnapshot();
-//     });
+        if (instance instanceof AccordionProvider) {
+            return instance.state.isItemExpanded(uuid);
+        }
+        throw new Error('could not find instance of AccordionProvider');
+    }
 
-//     it('renders correctly with different className', () => {
-//         const className = 'className';
-//         const wrapper = mountItem(
-//             <AccordionItemHeading className={className} />,
-//         );
-//         expect(wrapper.find('div').hasClass(className)).toEqual(true);
-//     });
+    it('renders correctly with min params', () => {
+        const wrapper = mountItem(
+            <AccordionItemHeading>
+                <div>Fake Title</div>
+            </AccordionItemHeading>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
 
-//     it('renders with different expandedClassName when item is expanded', () => {
-//         const expandedClassName = 'expandedClassName';
+    it('renders correctly with different className', () => {
+        const className = 'className';
+        const wrapper = mountItem(
+            <AccordionItemHeading className={className} />,
+        );
+        expect(wrapper.find('div').hasClass(className)).toEqual(true);
+    });
 
-//         const wrapper = mount(
-//             <AccordionProvider initialItems={[{ uuid: 0, expanded: true }]}>
-//                 <ItemProvider uuid={0}>
-//                     <AccordionItemHeading
-//                         expandedClassName={expandedClassName}
-//                     />
-//                 </ItemProvider>
-//             </AccordionProvider>,
-//         );
-//         expect(wrapper.find('div').hasClass(expandedClassName)).toEqual(true);
-//     });
+    it('renders with different expandedClassName when item is expanded', () => {
+        const expandedClassName = 'expandedClassName';
 
-//     it('toggles state when clicked', async () => {
-//         const wrapper = mountItem(
-//             <AccordionItemHeading>Fake Title</AccordionItemHeading>,
-//         );
+        const wrapper = mount(
+            <AccordionProvider preExpanded={[UUIDS.FOO]}>
+                <ItemProvider uuid={UUIDS.FOO}>
+                    <AccordionItemHeading
+                        expandedClassName={expandedClassName}
+                    />
+                </ItemProvider>
+            </AccordionProvider>,
+        );
+        expect(wrapper.find('div').hasClass(expandedClassName)).toEqual(true);
+    });
 
-//         expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
-//         wrapper.find('div').simulate('click');
-//         expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeTruthy();
-//     });
+    it('toggles state when clicked', async () => {
+        const wrapper = mountItem(
+            <AccordionItemHeading>Fake Title</AccordionItemHeading>,
+            UUIDS.FOO,
+        );
 
-//     it('toggles state when pressing enter', async () => {
-//         const wrapper = mountItem(
-//             <AccordionItemHeading>Fake Title</AccordionItemHeading>,
-//         );
+        expect(isExpanded(wrapper, UUIDS.FOO)).toBeFalsy();
+        wrapper.find('div').simulate('click');
+        expect(isExpanded(wrapper, UUIDS.FOO)).toBeTruthy();
+    });
 
-//         expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
-//         wrapper.find('div').simulate('keyDown', { which: 13 });
-//         expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeTruthy();
-//     });
+    it('toggles state when pressing enter', async () => {
+        const wrapper = mountItem(
+            <AccordionItemHeading>Fake Title</AccordionItemHeading>,
+            UUIDS.FOO,
+        );
 
-//     it('toggles state when pressing space', async () => {
-//         const wrapper = mountItem(
-//             <AccordionItemHeading>Fake Title</AccordionItemHeading>,
-//         );
+        expect(isExpanded(wrapper, UUIDS.FOO)).toBeFalsy();
+        wrapper.find('div').simulate('keyDown', { which: 13 });
+        expect(isExpanded(wrapper, UUIDS.FOO)).toBeTruthy();
+    });
 
-//         expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
-//         wrapper.find('div').simulate('keyDown', { which: 32 });
-//         expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeTruthy();
-//     });
+    it('toggles state when pressing space', async () => {
+        const wrapper = mountItem(
+            <AccordionItemHeading>Fake Title</AccordionItemHeading>,
+            UUIDS.FOO,
+        );
 
-//     it('doesn’t toggle state when pressing another key', async () => {
-//         const wrapper = mountItem(
-//             <AccordionItemHeading>Fake Title</AccordionItemHeading>,
-//         );
+        expect(isExpanded(wrapper, UUIDS.FOO)).toBeFalsy();
+        wrapper.find('div').simulate('keyDown', { which: 32 });
+        expect(isExpanded(wrapper, UUIDS.FOO)).toBeTruthy();
+    });
 
-//         expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
-//         wrapper.find('div').simulate('keyDown', { which: 8 });
-//         expect(isExpanded(wrapper, DEFAULT_ITEM.uuid)).toBeFalsy();
-//     });
+    it('doesn’t toggle state when pressing another key', async () => {
+        const wrapper = mountItem(
+            <AccordionItemHeading>Fake Title</AccordionItemHeading>,
+            UUIDS.FOO,
+        );
 
-//     it('respects arbitrary user-defined props', () => {
-//         const wrapper = mountItem(
-//             <AccordionItemHeading lang="en">Fake Title</AccordionItemHeading>,
-//         );
+        expect(isExpanded(wrapper, UUIDS.FOO)).toBeFalsy();
+        wrapper.find('div').simulate('keyDown', { which: 8 });
+        expect(isExpanded(wrapper, UUIDS.FOO)).toBeFalsy();
+    });
 
-//         const div = wrapper.find('div').getDOMNode();
+    it('respects arbitrary user-defined props', () => {
+        const wrapper = mountItem(
+            <AccordionItemHeading lang="en">Fake Title</AccordionItemHeading>,
+        );
 
-//         expect(div.getAttribute('lang')).toEqual('en');
-//     });
-// });
+        const div = wrapper.find('div').getDOMNode();
+
+        expect(div.getAttribute('lang')).toEqual('en');
+    });
+});
