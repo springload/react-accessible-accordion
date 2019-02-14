2f4b88f2f2cba406dec366d04c83d0ef1c5ee6fb
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-02-15 10:08:44 +1300

    Refactor tests in Accordion.spec

--- src/Accordion/Accordion.spec.tsx
+++ src/Accordion/Accordion.spec.tsx
@@ -1,258 +1,75 @@
-import { mount, ReactWrapper } from 'enzyme';
+import { mount } from 'enzyme';
 import * as React from 'react';
-import { Provider } from '../AccordionContext/AccordionContext';
-import { default as AccordionItem } from '../AccordionItem/AccordionItem.wrapper';
-import { default as AccordionItemHeading } from '../AccordionItemHeading/AccordionItemHeading.wrapper';
+import * as ReactTestRenderer from 'react-test-renderer';
+import { Provider, ProviderProps } from '../AccordionContext/AccordionContext';
+import { UUID } from '../ItemContext/ItemContext';
 import { default as Accordion } from './Accordion.wrapper';
 
-// describe('Accordion', () => {
-//     it('different className', () => {
-//         const wrapper = mount(<Accordion className="testCSSClass" />);
-//         expect(wrapper.find('div').props().className).toEqual('testCSSClass');
-//     });
+describe('Accordion', () => {
+    it('renders without erroring', () => {
+        expect(() => {
+            mount(<Accordion />);
+        }).not.toThrow();
+    });
 
-//     describe('<Accordion allowMultipleExpanded="false" />', () => {
-//         const [FooTitle, BarTitle]: React.SFC[] = [
-//             (): JSX.Element => <AccordionItemHeading>Foo</AccordionItemHeading>,
-//             (): JSX.Element => <AccordionItemHeading>Bar</AccordionItemHeading>,
-//         ];
+    describe('className', () => {
+        it('is “accordion” by default', () => {
+            expect(
+                mount(<Accordion />)
+                    .getDOMNode()
+                    .getAttribute('class'),
+            ).toBe('accordion');
+        });
 
-//         function mountAccordion(): ReactWrapper {
-//             return mount(
-//                 <Accordion>
-//                     <AccordionItem>
-//                         <FooTitle />
-//                     </AccordionItem>
-//                     <AccordionItem>
-//                         <BarTitle />
-//                     </AccordionItem>
-//                 </Accordion>,
-//             );
-//         }
+        it('can be overridden', () => {
+            const OVERRIDE = 'OVERRIDE';
+            expect(
+                mount(<Accordion className={OVERRIDE} />)
+                    .getDOMNode()
+                    .getAttribute('class'),
+            ).toBe(OVERRIDE);
+        });
+    });
 
-//         it('expands a collapsed item when its title is clicked', () => {
-//             const wrapper = mountAccordion();
+    describe('context provision', () => {
+        it('is given to descendants', () => {
+            expect(mount(<Accordion />).find(Provider).length).toEqual(1);
+        });
 
-//             wrapper.find(FooTitle).simulate('click');
+        it('respects all directly-proxied ProviderProps', () => {
+            // This test asserts that all ProviderProps are respected, AND
+            // the ProxiedProviderProps type helps to ensure that we haven't
+            // forgotten any.
 
-//             const provider = wrapper.find(Provider).instance() as Provider;
+            type ProxiedProviderProps = Pick<
+                Required<ProviderProps>,
+                Exclude<keyof ProviderProps, 'children'> // 'children' is not proxied directly, and is tested separately below.
+            >;
 
-//             expect(
-//                 provider.state.items.filter(
-//                     (item: Item) => item.expanded === true,
-//                 ).length,
-//             ).toEqual(1);
-//         });
+            const props: ProxiedProviderProps = {
+                preExpanded: ['foo', 'bar'],
+                allowMultipleExpanded: true,
+                allowZeroExpanded: true,
+                onChange: (args: UUID[]): void => {
+                    // noop
+                },
+            };
 
-//         it('expands a collapsed item when its title is clicked, and closes the others', () => {
-//             const wrapper = mountAccordion();
+            expect(
+                mount(<Accordion {...props} />)
+                    .find(Provider)
+                    .props(),
+            ).toMatchObject(props);
+        });
 
-//             wrapper.find(BarTitle).simulate('click');
-
-//             const instance = wrapper.find(Provider).instance() as Provider;
-
-//             expect(
-//                 instance.state.items.filter(
-//                     (item: Item) => item.expanded === true,
-//                 ).length,
-//             ).toEqual(1);
-//         });
-
-//         it('pre expanded accordion', () => {
-//             const wrapper = mount(
-//                 <Accordion>
-//                     <AccordionItem expanded={true}>Fake Child</AccordionItem>
-//                     <AccordionItem>Fake Child</AccordionItem>
-//                 </Accordion>,
-//             );
-
-//             const instance = wrapper.find(Provider).instance() as Provider;
-
-//             expect(
-//                 instance.state.items.filter(
-//                     (item: Item) => item.expanded === true,
-//                 ).length,
-//             ).toEqual(1);
-//         });
-
-//         it('works with multiple pre expanded accordion. Extra expands are just ignored.', () => {
-//             const expandedClassName = 'EXPANDED';
-//             const wrapper = mount(
-//                 <Accordion allowMultipleExpanded={false}>
-//                     <AccordionItem
-//                         expanded={true}
-//                         expandedClassName={expandedClassName}
-//                     >
-//                         Fake Child
-//                     </AccordionItem>
-//                     <AccordionItem
-//                         expanded={true}
-//                         expandedClassName={expandedClassName}
-//                     >
-//                         Fake Child
-//                     </AccordionItem>
-//                 </Accordion>,
-//             );
-
-//             const instance = wrapper.find(Provider).instance() as Provider;
-
-//             expect(
-//                 instance.state.items.filter((item: Item) => item.expanded)
-//                     .length,
-//             ).toEqual(1);
-//             expect(
-//                 wrapper.findWhere((item: ReactWrapper) =>
-//                     item.hasClass(expandedClassName),
-//                 ).length,
-//             ).toEqual(1);
-//         });
-
-//         it('respects arbitrary user-defined props', () => {
-//             const wrapper = mount(<Accordion lang="en" />);
-//             const div = wrapper.find('div').getDOMNode();
-
-//             expect(div.getAttribute('lang')).toEqual('en');
-//         });
-//     });
-
-//     describe('<Accordion allowMultipleExpanded="true" />', () => {
-//         const [FooTitle, BarTitle]: React.SFC[] = [
-//             (): JSX.Element => <AccordionItemHeading>Foo</AccordionItemHeading>,
-//             (): JSX.Element => <AccordionItemHeading>Bar</AccordionItemHeading>,
-//         ];
-
-//         function mountMultipleExpanded(): ReactWrapper {
-//             return mount(
-//                 <Accordion allowMultipleExpanded={true}>
-//                     <AccordionItem>
-//                         <FooTitle />
-//                     </AccordionItem>
-//                     <AccordionItem>
-//                         <BarTitle />
-//                     </AccordionItem>
-//                 </Accordion>,
-//             );
-//         }
-
-//         it('expands a collapsed item when its title is clicked', () => {
-//             const wrapper = mountMultipleExpanded();
-
-//             wrapper.find(FooTitle).simulate('click');
-
-//             const instance = wrapper.find(Provider).instance() as Provider;
-
-//             expect(
-//                 instance.state.items.filter(
-//                     (item: Item) => item.expanded === true,
-//                 ).length,
-//             ).toEqual(1);
-//         });
-
-//         it("expands a collapsed item when its title is clicked, and doesn't close the others", () => {
-//             const wrapper = mountMultipleExpanded();
-
-//             wrapper.find(FooTitle).simulate('click');
-//             wrapper.find(BarTitle).simulate('click');
-
-//             const instance = wrapper.find(Provider).instance() as Provider;
-
-//             expect(
-//                 instance.state.items.filter(
-//                     (item: Item) => item.expanded === true,
-//                 ).length,
-//             ).toEqual(2);
-//         });
-
-//         it('collapses an expanded item when its title is clicked and there is more than one item expanded', () => {
-//             const wrapper = mountMultipleExpanded();
-
-//             wrapper.find(FooTitle).simulate('click'); // open
-//             wrapper.find(BarTitle).simulate('click'); // open
-//             wrapper.find(FooTitle).simulate('click'); // close
-
-//             const instance = wrapper.find(Provider).instance() as Provider;
-
-//             expect(
-//                 instance.state.items.filter(
-//                     (item: Item) => item.expanded === true,
-//                 ).length,
-//             ).toEqual(1);
-//         });
-//     });
-
-//     it('pre expanded accordion when allowMultipleExpanded is true', () => {
-//         const wrapper = mount(
-//             <Accordion allowMultipleExpanded={true}>
-//                 <AccordionItem expanded={true}>Fake Child</AccordionItem>
-//                 <AccordionItem expanded={true}>Fake Child</AccordionItem>
-//             </Accordion>,
-//         );
-
-//         const instance = wrapper.find(Provider).instance() as Provider;
-
-//         expect(
-//             instance.state.items.filter((item: Item) => item.expanded === true)
-//                 .length,
-//         ).toEqual(2);
-//     });
-// });
-
-// describe('<Accordion allowZeroExpanded="false" />', () => {
-//     const FooTitle: React.SFC = (): JSX.Element => (
-//         <AccordionItemHeading>Foo</AccordionItemHeading>
-//     );
-
-//     function mountAccordion(): ReactWrapper {
-//         return mount(
-//             <Accordion>
-//                 <AccordionItem>
-//                     <FooTitle />
-//                 </AccordionItem>
-//             </Accordion>,
-//         );
-//     }
-
-//     it("doesn't collapse an expanded item when it's the only item expanded", () => {
-//         const wrapper = mountAccordion();
-
-//         wrapper.find(FooTitle).simulate('click'); // open
-//         wrapper.find(FooTitle).simulate('click'); // close
-
-//         const instance = wrapper.find(Provider).instance() as Provider;
-
-//         expect(
-//             instance.state.items.filter((item: Item) => item.expanded === true)
-//                 .length,
-//         ).toEqual(1);
-//     });
-// });
-
-// describe('<Accordion allowZeroExpanded="true" />', () => {
-//     const FooTitle: React.SFC = (): JSX.Element => (
-//         <AccordionItemHeading>Foo</AccordionItemHeading>
-//     );
-
-//     function mountZeroExpanded(): ReactWrapper {
-//         return mount(
-//             <Accordion allowZeroExpanded={true}>
-//                 <AccordionItem>
-//                     <FooTitle />
-//                 </AccordionItem>
-//             </Accordion>,
-//         );
-//     }
-
-//     it("collapses an expanded item when its title is clicked and it's the only item open", () => {
-//         const wrapper = mountZeroExpanded();
-
-//         wrapper.find(FooTitle).simulate('click'); // open
-//         wrapper.find(FooTitle).simulate('click'); // close
-
-//         const provider = wrapper.find(Provider).instance() as Provider;
-
-//         expect(
-//             provider.state.items.filter((item: Item) => item.expanded === true)
-//                 .length,
-//         ).toEqual(0);
-//     });
-// });
+        it('respects the ‘children’ prop', () => {
+            expect(
+                mount(
+                    <Accordion>
+                        <div data-foo={true} />
+                    </Accordion>,
+                ).find('[data-foo]'),
+            ).toHaveLength(1);
+        });
+    });
+});
