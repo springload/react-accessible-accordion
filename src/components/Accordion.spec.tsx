2dae2f2d22c26d1d581b6cb55514876a046c422c
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-03-05 21:39:32 +1300

    Add unit tests for expand and collapse functionality

--- src/components/Accordion.spec.tsx
+++ src/components/Accordion.spec.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
-import { cleanup, render } from 'react-testing-library';
+import { cleanup, fireEvent, render } from 'react-testing-library';
 import { default as Accordion } from './Accordion';
+import AccordionItem from './AccordionItem';
+import AccordionItemHeading from './AccordionItemHeading';
 
-const TEST_ID = 'TEST_ID';
+enum UUIDS {
+    FOO = 'FOO',
+    BAR = 'Bar',
+}
 
 describe('Accordion', () => {
     afterEach(() => {
@@ -17,27 +22,103 @@ describe('Accordion', () => {
 
     describe('className', () => {
         it('is “accordion” by default', () => {
-            const { getByTestId } = render(<Accordion data-testid={TEST_ID} />);
-            expect(getByTestId(TEST_ID).className).toBe('accordion');
+            const { getByTestId } = render(
+                <Accordion data-testid={UUIDS.FOO} />,
+            );
+            expect(getByTestId(UUIDS.FOO).className).toBe('accordion');
         });
 
         it('can be overridden', () => {
             const { getByTestId } = render(
-                <Accordion className="foo" data-testid={TEST_ID} />,
+                <Accordion className="foo" data-testid={UUIDS.FOO} />,
             );
-            expect(getByTestId(TEST_ID).className).toBe('foo');
+            expect(getByTestId(UUIDS.FOO).className).toBe('foo');
         });
     });
 
-    describe('children', () => {
-        it('respects the children prop', () => {
-            const { queryAllByTestId } = render(
-                <Accordion>
-                    <div data-testid={TEST_ID} />
-                    <div data-testid={TEST_ID} />
-                </Accordion>,
-            );
-            expect(queryAllByTestId(TEST_ID).length).toBe(2);
+    describe('expanding and collapsing: ', () => {
+        describe('allowMultipleExpanded prop', () => {
+            it('permits multiple items to be expanded when explicitly true', () => {
+                const { getByTestId } = render(
+                    <Accordion allowMultipleExpanded={true}>
+                        <AccordionItem>
+                            <AccordionItemHeading data-testid={UUIDS.FOO} />
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading data-testid={UUIDS.BAR} />
+                        </AccordionItem>
+                    </Accordion>,
+                );
+
+                fireEvent.click(getByTestId(UUIDS.FOO));
+                fireEvent.click(getByTestId(UUIDS.BAR));
+
+                expect(
+                    getByTestId(UUIDS.FOO).getAttribute('aria-expanded'),
+                ).toEqual('true');
+                expect(
+                    getByTestId(UUIDS.BAR).getAttribute('aria-expanded'),
+                ).toEqual('true');
+            });
+
+            it('prevents multiple items being expanded by default', () => {
+                const { getByTestId } = render(
+                    <Accordion>
+                        <AccordionItem>
+                            <AccordionItemHeading data-testid={UUIDS.FOO} />
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading data-testid={UUIDS.BAR} />
+                        </AccordionItem>
+                    </Accordion>,
+                );
+
+                fireEvent.click(getByTestId(UUIDS.FOO));
+                fireEvent.click(getByTestId(UUIDS.BAR));
+
+                expect(
+                    getByTestId(UUIDS.FOO).getAttribute('aria-expanded'),
+                ).toEqual('false');
+                expect(
+                    getByTestId(UUIDS.BAR).getAttribute('aria-expanded'),
+                ).toEqual('true');
+            });
+        });
+
+        describe('allowZeroExpanded prop', () => {
+            it('permits the last-expanded item to be collapsed when explicitly true', () => {
+                const { getByTestId } = render(
+                    <Accordion allowZeroExpanded={true}>
+                        <AccordionItem>
+                            <AccordionItemHeading data-testid={UUIDS.FOO} />
+                        </AccordionItem>
+                    </Accordion>,
+                );
+
+                fireEvent.click(getByTestId(UUIDS.FOO));
+                fireEvent.click(getByTestId(UUIDS.FOO));
+
+                expect(
+                    getByTestId(UUIDS.FOO).getAttribute('aria-expanded'),
+                ).toEqual('false');
+            });
+
+            it('prevents the last-expanded item being collapsed by default', () => {
+                const { getByTestId } = render(
+                    <Accordion>
+                        <AccordionItem>
+                            <AccordionItemHeading data-testid={UUIDS.FOO} />
+                        </AccordionItem>
+                    </Accordion>,
+                );
+
+                fireEvent.click(getByTestId(UUIDS.FOO));
+                fireEvent.click(getByTestId(UUIDS.FOO));
+
+                expect(
+                    getByTestId(UUIDS.FOO).getAttribute('aria-expanded'),
+                ).toEqual('true');
+            });
         });
     });
 });
