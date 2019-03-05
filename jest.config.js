f888a9b6e6e11485166e908406cb6aae53aa3dc2
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-03-05 22:59:52 +1300

    Remove enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-    setupFiles: ['./src/setupTests.js'],
-    setupFilesAfterEnv: ['./src/setupTestFramework.js'],
     testPathIgnorePatterns: ['/node_modules/', '/dist/', '/integration/'],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     transform: {
--- src/setupTests.js
+++
@@ -1,4 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16.3';
-
-configure({ adapter: new Adapter() });
