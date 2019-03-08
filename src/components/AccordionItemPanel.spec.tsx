fd9aaf443374bd83da72bb4f0825c057a499587b
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-03-08 15:53:57 +1300

    Remove 'expandedClassName' from 'AccordionItemPanel'

--- src/components/AccordionItemPanel.spec.tsx
+++ src/components/AccordionItemPanel.spec.tsx
@@ -6,7 +6,7 @@ import AccordionItemPanel from './AccordionItemPanel';
 
 enum UUIDS {
     FOO = 'FOO',
-    BAR = 'Bar',
+    BAR = 'BAR',
 }
 
 describe('AccordionItem', () => {
@@ -20,53 +20,34 @@ describe('AccordionItem', () => {
         }).not.toThrow();
     });
 
-    describe('className + expandedClassName', () => {
-        it('are “BEM” by default', () => {
+    describe('className prop', () => {
+        it('is “BEM” by default', () => {
             const { getByTestId } = render(
-                <Accordion preExpanded={[UUIDS.FOO]}>
+                <Accordion>
                     <AccordionItem uuid={UUIDS.FOO}>
                         <AccordionItemPanel data-testid={UUIDS.FOO} />
-                    </AccordionItem>
-                    <AccordionItem uuid={UUIDS.BAR}>
-                        <AccordionItemPanel data-testid={UUIDS.BAR} />
                     </AccordionItem>
                 </Accordion>,
             );
 
             expect(Array.from(getByTestId(UUIDS.FOO).classList)).toEqual([
-                'accordion__panel',
-                'accordion__panel--expanded',
-            ]);
-            expect(Array.from(getByTestId(UUIDS.BAR).classList)).toEqual([
                 'accordion__panel',
             ]);
         });
 
         it('can be overridden', () => {
             const { getByTestId } = render(
-                <Accordion preExpanded={[UUIDS.FOO]}>
+                <Accordion>
                     <AccordionItem uuid={UUIDS.FOO}>
                         <AccordionItemPanel
                             data-testid={UUIDS.FOO}
                             className="foo"
-                            expandedClassName="foo--expanded"
-                        />
-                    </AccordionItem>
-                    <AccordionItem uuid={UUIDS.BAR}>
-                        <AccordionItemPanel
-                            data-testid={UUIDS.BAR}
-                            className="foo"
-                            expandedClassName="foo--expanded"
                         />
                     </AccordionItem>
                 </Accordion>,
             );
 
             expect(Array.from(getByTestId(UUIDS.FOO).classList)).toEqual([
-                'foo',
-                'foo--expanded',
-            ]);
-            expect(Array.from(getByTestId(UUIDS.BAR).classList)).toEqual([
                 'foo',
             ]);
         });
