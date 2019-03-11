6e0d77cfdb2ef9831946a151b03a2707ed12ffe1
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-03-12 10:34:06 +1300

    Add validation to AccordionItemHeading

--- src/components/AccordionItemHeading.tsx
+++ src/components/AccordionItemHeading.tsx
@@ -12,14 +12,54 @@ const defaultProps = {
     'aria-level': 3,
 };
 
+export const SPEC_ERROR = `AccordionItemButton may contain only one child element, which must be an instance of AccordionItemButton.
+
+From the WAI-ARIA spec (https://www.w3.org/TR/wai-aria-practices-1.1/#accordion):
+
+“The button element is the only element inside the heading element. That is, if there are other visually persistent elements, they are not included inside the heading element.”
+
+`;
+
 export class AccordionItemHeading extends React.PureComponent<Props> {
     static defaultProps: typeof defaultProps = defaultProps;
+
+    ref: HTMLDivElement | undefined;
+
+    static VALIDATE(ref: HTMLDivElement | undefined): void | never {
+        if (ref === undefined) {
+            throw new Error('ref is undefined');
+        }
+        if (
+            !(
+                ref.childElementCount === 1 &&
+                ref.firstElementChild &&
+                ref.firstElementChild.getAttribute(
+                    'data-accordion-component',
+                ) === 'AccordionItemButton'
+            )
+        ) {
+            throw new Error(SPEC_ERROR);
+        }
+    }
+
+    setRef = (ref: HTMLDivElement): void => {
+        this.ref = ref;
+    };
+
+    componentDidUpdate(): void {
+        AccordionItemHeading.VALIDATE(this.ref);
+    }
+
+    componentDidMount(): void {
+        AccordionItemHeading.VALIDATE(this.ref);
+    }
 
     render(): JSX.Element {
         return (
             <div
                 data-accordion-component="AccordionItemHeading"
                 {...this.props}
+                ref={this.setRef}
             />
         );
     }
