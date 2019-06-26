1316ca967dd2db60876eb57cb7372b0943076012
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-06-27 10:42:06 +1200

    Bump react-testing-library

--- src/components/AccordionItemPanel.spec.tsx
+++ src/components/AccordionItemPanel.spec.tsx
@@ -1,5 +1,5 @@
+import { cleanup, render } from '@testing-library/react';
 import * as React from 'react';
-import { cleanup, render } from 'react-testing-library';
 import Accordion from './Accordion';
 import AccordionItem from './AccordionItem';
 import AccordionItemPanel from './AccordionItemPanel';
--- src/components/AccordionItemState.spec.tsx
+++ src/components/AccordionItemState.spec.tsx
@@ -1,5 +1,5 @@
+import { render } from '@testing-library/react';
 import * as React from 'react';
-import { render } from 'react-testing-library';
 import Accordion from './Accordion';
 import AccordionItem from './AccordionItem';
 import AccordionItemState from './AccordionItemState';
