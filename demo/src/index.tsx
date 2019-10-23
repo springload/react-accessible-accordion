f0760d36006b2021adb01de6e1a04e68a53f33b5
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2019-10-23 16:58:51 +1300

    Springload logo update.

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -16,6 +16,9 @@ import {
 // tslint:disable-next-line no-import-side-effect
 import './main.css';
 
+// tslint:disable-next-line no-import-side-effect
+import './springload.svg';
+
 // tslint:disable-next-line no-import-side-effect ordered-imports
 import '../../src/css/fancy-example.css';
 
