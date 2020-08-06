a43f8318a4e9c40de84eb41d5421f0660974827a
Matthew Holloway <matthew@springload.co.nz>
2020-08-06 14:31:17 +1200

    AccordionItem preserving uuid on instance

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import DisplayName from '../helpers/DisplayName';
 import { DivAttributes } from '../helpers/types';
 import { assertValidHtmlId, nextUuid } from '../helpers/uuid';
 import {
@@ -9,22 +10,34 @@ import {
 } from './ItemContext';
 
 type Props = DivAttributes & {
-    className?: string;
     uuid?: UUID;
     activeClassName?: string;
     dangerouslySetExpanded?: boolean;
 };
 
-const AccordionItem = ({
-    uuid = nextUuid(),
-    className = 'accordion__item',
-    activeClassName,
-    dangerouslySetExpanded,
-    ...rest
-}: Props): JSX.Element => {
-    const renderChildren = (itemContext: ItemContext): JSX.Element => {
+const defaultProps = {
+    className: 'accordion__item',
+};
+
+export default class AccordionItem extends React.Component<Props> {
+    static defaultProps: typeof defaultProps = defaultProps;
+
+    static displayName: DisplayName.AccordionItem = DisplayName.AccordionItem;
+
+    instanceUuid: UUID = nextUuid();
+
+    renderChildren = (itemContext: ItemContext): JSX.Element => {
+        const {
+            uuid,
+            className,
+            activeClassName,
+            dangerouslySetExpanded,
+            ...rest
+        } = this.props;
         const { expanded } = itemContext;
         const cx = expanded && activeClassName ? activeClassName : className;
+
+        console.log({ rest });
 
         return (
             <div
@@ -35,18 +48,26 @@ const AccordionItem = ({
         );
     };
 
-    if (rest.id) {
-        assertValidHtmlId(rest.id);
+    render(): JSX.Element {
+        const {
+            uuid = this.instanceUuid,
+            dangerouslySetExpanded,
+            ...rest
+        } = this.props;
+
+        assertValidHtmlId(uuid);
+
+        if (rest.id) {
+            assertValidHtmlId(rest.id);
+        }
+        ``;
+        return (
+            <ItemProvider
+                uuid={uuid}
+                dangerouslySetExpanded={dangerouslySetExpanded}
+            >
+                <ItemConsumer>{this.renderChildren}</ItemConsumer>
+            </ItemProvider>
+        );
     }
-
-    return (
-        <ItemProvider
-            uuid={uuid}
-            dangerouslySetExpanded={dangerouslySetExpanded}
-        >
-            <ItemConsumer>{renderChildren}</ItemConsumer>
-        </ItemProvider>
-    );
-};
-
-export default AccordionItem;
+}
